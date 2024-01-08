7a3c5e71a8c75cf305624d879e4ae702b97525e4
Sarah <sarah.emmoth@proton.me>
2024-01-08 11:35:40 +0100

    header section wip, changes to headings, small layout changes, etc. big update...

--- scripts.js
+++ scripts.js
@@ -1,33 +1,50 @@
 const checkbox = document.getElementById("checkbox");
 
 checkbox.addEventListener("change", () => {
-  document.body.classList.toggle("lightToDark")
-})
+  document.body.classList.toggle("lightToDark");
+});
 
-AOS.init();
+// Function to update the date and time
+function updateDateTime() {
+  var options = { 
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+};
+  var currentDate = new Date();
+  var dateTimeString = currentDate.toLocaleString('en-GB', options); 
+  document.getElementById("dateTime").innerHTML = dateTimeString;
+}
+
+// Initial call to display date and time
+updateDateTime();
+
+// Update the date and time every second (1000 milliseconds)
+setInterval(updateDateTime, 1000);
+
+window.addEventListener('load', function () {
+  AOS.init();
+});
 
 // You can also pass an optional settings object
 // below listed default settings
 AOS.init({
   // Global settings:
   disable: window.innerWidth < 1000, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
-  startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
-  initClassName: 'aos-init', // class applied after initialization
-  animatedClassName: 'aos-animate', // class applied on animation
+  startEvent: "DOMContentLoaded", // name of the event dispatched on the document, that AOS should initialize on
+  initClassName: "aos-init", // class applied after initialization
+  animatedClassName: "aos-animate", // class applied on animation
   useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
   disableMutationObserver: false, // disables automatic mutations' detections (advanced)
   debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
   throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
-  
 
   // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
   offset: 120, // offset (in px) from the original trigger point
   delay: 0, // values from 0 to 3000, with step 50ms
   duration: 1000, // values from 0 to 3000, with step 50ms
-  easing: 'ease', // default easing for AOS animations
+  easing: "ease", // default easing for AOS animations
   once: true, // whether animation should happen only once - while scrolling down
   mirror: false, // whether elements should animate out while scrolling past them
-  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
-
+  anchorPlacement: "top-bottom", // defines which position of the element regarding to window should trigger the animation
 });
-
