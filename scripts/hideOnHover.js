f0c27e189e2d15a060f7da39653d7d41aa13e604
sarah <sarah.emmoth@proton.me>
2024-02-20 11:29:29 +0100

    remove comments

--- scripts/hideOnHover.js
+++ scripts/hideOnHover.js
@@ -1,14 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
   const hideContent = document.querySelector(".hide");
-  hideContent.style.opacity = "0"; // Set initial opacity to '0'
+  hideContent.style.opacity = "0";
 
   const bento1 = document.querySelector(".bento1");
 
   bento1.addEventListener("mouseenter", function() {
-    hideContent.style.opacity = "1"; // Fade in on mouse enter
+    hideContent.style.opacity = "1"; 
   });
 
   bento1.addEventListener("mouseleave", function() {
-    hideContent.style.opacity = "0"; // Fade out on mouse leave
+    hideContent.style.opacity = "0"; 
   });
 });
